94c8624d7cc8f0eecd2a3e9b1e7b980ba590676f
Kevin Johnson <kevinJohnson190306@gmail.com>
2024-12-09 18:26:25 +0000

    Speed optimizations

--- Day3/Q1.py
+++ Day3/Q1.py
@@ -1,21 +1,24 @@
-with open("input.txt") as file:
+from PerformanceTester.PerformanceTester import test_time
+
+def main():
     result = 0
-
-    last_character = None
     current_string = ""
-
-    for character in file.read().replace("\n", ""):
-        if character == ")":
-            if current_string[:4] == "mul(":
-                numbers = current_string[4:].split(",")
-                if len(numbers) == 2:
-                    num1 = numbers[0]
-                    num2 = numbers[1]
-                    if num1.isnumeric() and num2.isnumeric():
-                        result += int(num1) * int(num2)
-        elif character == "m":
-            current_string = ""
-            
-        current_string += character
-
+    with open("input.txt") as file:
+        for character in file.read():
+            if character == ")":
+                if current_string[:4] == "mul(":
+                    numbers = [current_string[4:].split(",")]
+                    if len(numbers) == 2:
+                        num1 = numbers[0]
+                        num2 = numbers[1]
+                        if num1.isnumeric() and num2.isnumeric():
+                            result += int(num1) * int(num2)
+                current_string = ""
+            elif character == "m":
+                current_string = "m"
+            else:
+                current_string += character
     print(result)
+
+if __name__ == "__main__":
+    test_time(main)
