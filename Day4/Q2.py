60b39db7549e207b546b915e9c0ad4580cb6aeed
Kevin Johnson <kevinJohnson190306@gmail.com>
2024-12-09 19:07:42 +0000

    Speed optimizations

--- Day4/Q2.py
+++ Day4/Q2.py
@@ -1,19 +1,23 @@
-with open("input.txt") as file:
+from PerformanceTester.PerformanceTester import test_time
 
-    lines = file.readlines()
-
+def main():
     num_x_mas = 0
-    
-    for y in range(1, len(lines)-1):
-        line = lines[y]
-        for x in range(1, len(lines[y])-1):
-            if line[x] == "A":
-                if (
-                    ((lines[y-1][x-1] == "M" and lines[y+1][x+1] == "S") or
-                    (lines[y-1][x-1] == "S" and lines[y+1][x+1] == "M"))
-                    and
-                    ((lines[y+1][x-1] == "M" and lines[y-1][x+1] == "S") or
-                    (lines[y+1][x-1] == "S" and lines[y-1][x+1] == "M"))
-                ):
-                    num_x_mas += 1
+    with open("input.txt") as file:
+        lines = file.readlines()
+        h,w = len(lines)-1, len(lines[0])-1
+        for y in range(1, h):
+            line = lines[y]
+            for x in range(1, w):
+                if line[x] == "A":
+                    if (
+                        ((lines[y-1][x-1] == "M" and lines[y+1][x+1] == "S") or
+                        (lines[y-1][x-1] == "S" and lines[y+1][x+1] == "M"))
+                        and
+                        ((lines[y+1][x-1] == "M" and lines[y-1][x+1] == "S") or
+                        (lines[y+1][x-1] == "S" and lines[y-1][x+1] == "M"))
+                    ):
+                        num_x_mas += 1
     print(num_x_mas)
+
+if __name__ == "__main__":
+    test_time(main)
