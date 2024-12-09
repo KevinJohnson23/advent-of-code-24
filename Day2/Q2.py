cb5623f2e082dc43ecc604ca86db00941dacb93c
Kevin Johnson <kevinJohnson190306@gmail.com>
2024-12-09 18:08:51 +0000

    Speed optimizations

--- Day2/Q2.py
+++ Day2/Q2.py
@@ -1,38 +1,40 @@
+from PerformanceTester.PerformanceTester import test_time
+
 def is_safe(numbers):
-    last_number = numbers[0]
+    last_number = None
     ascending = None
-    safe = True
-                
-    for n in numbers[1:]:
+
+    for n in numbers:
+        if last_number is None:
+            last_number = n
+            continue
         n_larger = n > last_number
-        if ascending == None:
-                        ascending = n_larger
+        if ascending is None:
+            ascending = n_larger
         else:
-            if ascending and not n_larger or n_larger and not ascending:
-                safe = False
-                break
-        difference = n - last_number if ascending else last_number - n
+            if ascending != n_larger:
+                return False
+        difference = abs(n - last_number)
         if difference < 1 or difference > 3:
-            safe = False
-            break
+            return False
         last_number = n
-                    
-    return safe
+    return True
 
-with open("input.txt") as file:
-    num_safe = 0
+def main():
+    with open("input.txt") as file:
+        num_safe = 0
+        for line in file:
+            numbers = [int(n.strip()) for n in line.split()]
+            if is_safe(numbers):
+                num_safe += 1
+            else:
+                for i in range(len(numbers)):
+                    n = numbers.pop(i)
+                    if is_safe(numbers):
+                        num_safe += 1
+                        break
+                    numbers.insert(i, n)
+        print(num_safe)
 
-    for line in file.readlines():
-        numbers = [int(n) for n in line.split(" ")]
-
-        if is_safe(numbers):
-            num_safe += 1
-        else:
-            for i in range(len(numbers)):
-                numbers_copy = numbers.copy()
-                numbers_copy.pop(i)
-                if is_safe(numbers_copy):
-                    num_safe += 1
-                    break
-                
-    print(num_safe)
+if __name__ == "__main__":
+    test_time(main)
