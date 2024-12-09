db1b0e14290f8235d17fc96603584962cfe881dd
Kevin Johnson <kevinJohnson190306@gmail.com>
2024-12-09 20:37:16 +0000

    Speed optimizations

--- Day6/Q1.py
+++ Day6/Q1.py
@@ -1,61 +1,48 @@
+from PerformanceTester.PerformanceTester import test_time
+
+obs = {"^":0,
+       ">":1,
+       "v":2,
+       "<":3}
+dirs = [
+    (0, -1),
+    (1, 0),
+    (0, 1),
+    (-1, 0)
+]
+
 def has_obstacle(path, x, y, w, h):
     if x in [-1, w] or y in [-1, h]:
         return False
     return path[y][x] == "#"
 
-def get_distinct(path, start_x, start_y, w, h):
-    current_x, current_y = start_x, start_y
+def get_distinct(path, current_x, current_y, current_idx, w, h):
     distinct = dict()
     while True:
-        dx, dy = 0, 0
-        distinct[current_x, current_y] = 1
-        current = path[current_y][current_x]
-        match current:
-            case "^":
-                if has_obstacle(path, current_x, current_y-1, w, h):
-                    path[current_y][current_x] = ">"
-                    continue
-                dy = -1
-            case ">":
-                if has_obstacle(path, current_x+1, current_y, w, h):
-                    path[current_y][current_x] = "v"
-                    continue
-                dx = 1
-            case "v":
-                if has_obstacle(path, current_x, current_y+1, w, h):
-                    path[current_y][current_x] = "<"
-                    continue
-                dy = 1
-            case "<":
-                if has_obstacle(path, current_x-1, current_y, w, h):
-                    path[current_y][current_x] = "^"
-                    continue
-                dx = -1
-        
-        if current_x+dx in [-1, w] or current_y+dy in [-1, h]:
+        if current_x in [-1, w] or current_y in [-1, h]:
             break
+        distinct[current_x, current_y] = 1
 
-        path[current_y][current_x] = "."
-        current_y += dy
-        current_x += dx
-        path[current_y][current_x] = current
+        while has_obstacle(path, current_x + dirs[current_idx][0], current_y + dirs[current_idx][1], w, h):
+            current_idx = (current_idx + 1) % 4
+        current_y += dirs[current_idx][1]
+        current_x += dirs[current_idx][0]
 
     return len(distinct.keys())
-    
-with open("input.txt") as file:
-    start_x = 0
-    start_y = 0
 
-    lines = file.readlines()
-    h = len(lines)
-    w = 0
-    for y in range(h):
-        lines[y] = list(lines[y].replace("\n", ""))
-        if w == 0:
-            w = len(lines[y])
-        for x in range(w):
-            if lines[y][x] in "^<>v":
-                start_x = x
-                start_y = y
+def main():
+    with open("input.txt") as file:
+        lines = file.readlines()
+        h = len(lines)
+        w = len(lines[0])
+        for y in range(h):
+            for x in range(w):
+                if lines[y][x] in "^<>v":
+                    start_x = x
+                    start_y = y
+                    start_idx = obs[lines[y][x]]
+                    print(get_distinct(lines, start_x, start_y, start_idx, w, h))
+                    return
 
-    print(get_distinct(lines, start_x, start_y, w, h))
+if __name__ == "__main__":
+    test_time(main)
