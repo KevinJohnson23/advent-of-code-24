c3f1e3b1ef6a3ff37fcf4da17f3b0adf7bdb36aa
Kevin Johnson <kevinJohnson190306@gmail.com>
2024-12-09 16:54:48 +0000

    Speed optimizations

--- Day1/Q1.py
+++ Day1/Q1.py
@@ -1,22 +1,19 @@
 from PerformanceTester.PerformanceTester import test_time
 
 def main():
+    left, right = [], []
     with open("input.txt") as file:
-        left = []
-        right = []
-        for line in file.readlines():
-            line_split = [int(x.strip()) for x in line.split()]
-            left.append(line_split[0])
-            right.append(line_split[1])
+        for line in file:
+            l, r = map(int, line.split())
+            left.append(l)
+            right.append(r)
+    left.sort()
+    right.sort()
 
-        left.sort()
-        right.sort()
-
-        total = 0
-        for i in range(len(left)):
-            total += abs(left[i] - right[i])
-
-        print(total)
+    total = 0
+    for i in range(len(left)):
+        total += abs(left[i] - right[i])
+    print(total)
 
 if __name__ == "__main__":
     test_time(main)
