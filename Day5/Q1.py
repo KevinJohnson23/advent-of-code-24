21d8a3b19c01cb4b54330c97d7581e7c74916a6d
Kevin Johnson <kevinJohnson190306@gmail.com>
2024-12-09 19:22:26 +0000

    Speed optimizations

--- Day5/Q1.py
+++ Day5/Q1.py
@@ -1,21 +1,24 @@
-with open("input.txt") as file:
+from PerformanceTester.PerformanceTester import test_time
 
-    lines = file.read().split("\n\n")
-    rules = [[int(x) for x in y.split("|")] for y in lines[0].split("\n")]
-    updates = [[int(x) for x in y.split(",")] for y in lines[1].split("\n")]
-
+def main():
     result = 0
+    with open("input.txt") as file:
+        lines = file.read().split("\n\n")
+        rules = [[int(x) for x in y.split("|")] for y in lines[0].splitlines()]
+        updates = [list(map(int, y.split(","))) for y in lines[1].splitlines()]
 
-    for update in updates:
-        correct = True
-        for rule in rules:
-            small = rule[0]
-            big = rule[1]
-            if small in update and big in update:
-                if update.index(small) > update.index(big):
-                    correct = False
-                    break
-        if correct:
-            result += update[len(update)//2]
-
+        for update in updates:
+            correct = True
+            for rule in rules:
+                small = rule[0]
+                big = rule[1]
+                if small in update and big in update:
+                    if update.index(small) > update.index(big):
+                        correct = False
+                        break
+            if correct:
+                result += update[len(update)//2]
     print(result)
+
+if __name__ == "__main__":
+    test_time(main)
