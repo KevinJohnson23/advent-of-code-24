ef2680ff85219d9c91873f91a396d9b590c05eb7
Kevin Johnson <kevinJohnson190306@gmail.com>
2024-12-10 20:30:07 +0000

    Speed optimizations

---  Day8/Q1.py
+++  Day8/Q1.py
@@ -1,33 +1,40 @@
-from itertools import product
+from PerformanceTester.PerformanceTester import test_time
 
 def is_inside(x, y, w, h):
-    return x >= 0 and x < w and y >= 0 and y < h
+    return 0 <= x < w and 0 <= y < h
 
-with open("input.txt") as file:
-    grid = [list(x.strip()) for x in file.readlines()]
-    w, h = len(grid[0]), len(grid)
-    positions = list(product(range(w), range(h)))
-    
+def main():
+    with open("input.txt") as file:
+        grid = [x.strip()for x in file]
+        w, h = len(grid[0]), len(grid)
+
     antinodes = set()
+    node_positions = dict()
 
-    for x, y in positions:
-        cell = grid[y][x]
+    for y in range(h):
+        for x in range(w):
+            cell = grid[y][x]
+            if cell.isalnum():
+                if cell in node_positions:
+                    node_positions[cell].append((x, y))
+                else:
+                    node_positions[cell] = [(x, y)]
 
-        if cell.isalnum():
+    for node, positions in node_positions.items():
+        for i in range(len(positions)):
+            for j in range(i+1, len(positions)):
+                x, y = positions[i]
+                x1, y1 = positions[j]
+                dx, dy = x1-x, y1-y
+                px, py = x-dx, y-dy
+                px1, py1 = x1+dx, y1+dy
 
-            for x1, y1 in positions:
-                if (x, y) == (x1, y1):
-                    continue
-
-                cell1 = grid[y1][x1]
-                if cell == cell1:
-                    dx, dy = x1-x, y1-y
-                    px, py = x-dx, y-dy
-                    px1, py1 = x1+dx, y1+dy
-
-                    if is_inside(px, py, w, h):
-                        antinodes.add((px, py))
-                    if is_inside(px1, py1, w, h):
-                        antinodes.add((px1, py1))
+                if is_inside(px, py, w, h):
+                    antinodes.add((px, py))
+                if is_inside(px1, py1, w, h):
+                    antinodes.add((px1, py1))
 
     print(len(antinodes))
+
+if __name__ == "__main__":
+    test_time(main)
