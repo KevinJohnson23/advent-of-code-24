7b3fc849a9a8332047060650c108fdcf78ceaee9
Kevin Johnson <kevinJohnson190306@gmail.com>
2024-12-11 22:05:29 +0000

    Speed optimizations

---  Day9/Q1.py
+++  Day9/Q1.py
@@ -1,33 +1,43 @@
-with open("input.txt") as file:
-    disk_map = file.read().strip()
+from PerformanceTester.PerformanceTester import test_time
+
+def main():
+    with open("input.txt") as file:
+        disk_map = file.read().strip()
 
     file_layout = []
+    free_intervals = []
     current_id = 0
+
     for i in range(len(disk_map)):
         char = int(disk_map[i])
         if i % 2 == 0:
-            for j in range(char):
-                file_layout.append(current_id)
+            file_layout.extend([current_id] * char)
             current_id += 1
         else:
-            for j in range(char):
-                file_layout.append(None)
+            current_num_files = len(file_layout)
+            free_intervals.append((current_num_files, current_num_files+char))
+            file_layout.extend([None] * char)
 
-    last_file_position = len(file_layout)-1
-    for i in range(len(file_layout)):
-        if file_layout[i] == None:
-            while file_layout[last_file_position] == None:
+    last_file_position = len(file_layout) - 1
+    for start, end in free_intervals:
+        for i in range(start, end):
+            while file_layout[last_file_position] is None:
                 last_file_position -= 1
             if last_file_position <= i:
                 break
             file_layout[i] = file_layout[last_file_position]
             file_layout[last_file_position] = None
+        if last_file_position <= i:
+            break
 
     result = 0
     for i in range(len(file_layout)):
-        if file_layout[i] != None:
-            result += i*file_layout[i]
-        else:
+        if file_layout[i] is None:
             break
+        else:
+            result += i * file_layout[i]
 
     print(result)
+
+if __name__ == "__main__":
+    test_time(main)
---  Day9/Q2.py
+++  Day9/Q2.py
@@ -1,77 +1,88 @@
-def get_free_intervals(file_layout):
-    free_intervals = []
+from PerformanceTester.PerformanceTester import test_time
 
-    free_start = None
-    free_end = None
-
-    for i in range(len(file_layout)):
-        if file_layout[i] == None:
-            free_start = i if free_start == None else free_start
-            free_end = i
-
-        elif free_start != None:
-
-            free_intervals.append([free_start, free_end])
-
-            free_start = None
-            free_end = None
-
-    return free_intervals
-
-with open("input.txt") as file:
-    disk_map = file.read().strip()
-
+def create_file_layout(disk_map):
     file_layout = []
+    free_intervals_size_map = dict()
+    file_intervals = []
     current_id = 0
+
     for i in range(len(disk_map)):
         char = int(disk_map[i])
         if i % 2 == 0:
-            for j in range(char):
-                file_layout.append(current_id)
+            file_intervals.insert(0, (len(file_layout), len(file_layout) + char - 1))
+            file_layout.extend([current_id] * char)
             current_id += 1
         else:
-            for j in range(char):
-                file_layout.append(None)
+            current_num_files = len(file_layout)
+            if char > 0:
+                free_interval = [current_num_files, current_num_files + char - 1]
+                if char in free_intervals_size_map:
+                    free_intervals_size_map[char].append(free_interval)
+                else:
+                    free_intervals_size_map[char] = [free_interval]
 
-    file_start = None
-    file_end = None
+            file_layout.extend([None] * char)
 
-    free_intervals = get_free_intervals(file_layout)
+    return file_layout, free_intervals_size_map, file_intervals
 
-    for j in range(len(file_layout)-1, -1, -1):
-        if file_start == None or file_layout[file_start] == None:
-            file_start = j
-        if file_start != None and file_layout[j] != None and file_layout[file_start] == file_layout[j]:
-            file_end = j
+def get_free_size(free_intervals_size_map, file_size, file_end):
+    free_size = None
+    for free_interval_size in free_intervals_size_map:
+        if free_interval_size < file_size:
+            continue
+        free_intervals = free_intervals_size_map[free_interval_size]
+        if len(free_intervals) == 0 or free_intervals[0][0] >= file_end:
+            continue
+        if free_size is None or free_intervals_size_map[free_interval_size][0][0] < \
+                free_intervals_size_map[free_size][0][0]:
+            free_size = free_interval_size
+    return free_size
 
-        if file_start != None and file_layout[j] != file_layout[file_start]:
+def main():
+    with open("input.txt") as file:
+        disk_map = file.read().strip()
 
-            file_length = file_start-file_end
+    file_layout, free_intervals_size_map, file_intervals = create_file_layout(disk_map)
 
-            for i in range(len(free_intervals)):
-                free_interval = free_intervals[i]
-                if free_interval == None or free_interval[0] >= file_end:
-                    continue
-                free_start = free_interval[0]
-                free_end = min(free_interval[1], file_end-1)
+    for file_start, file_end in file_intervals:
 
-                free_length = free_end-free_start
-                if free_length >= file_length:
+        file_size = file_end - file_start + 1
+        free_size = get_free_size(free_intervals_size_map, file_size, file_end)
+        if not free_size:
+            continue
+        free_interval = free_intervals_size_map[free_size].pop(0)
 
-                    for k in range(file_length+1):
-                        file_layout[free_start+k] = file_layout[file_start-k]
-                        file_layout[file_start-k] = None
-                    free_intervals[i][0] += file_length+1
-                    if free_intervals[i][0] > free_intervals[i][1]:
-                        free_intervals[i] = None
-                    break
+        for i in range(file_size):
+            file_layout[free_interval[0]+i] = file_layout[file_start+i]
+            file_layout[file_start+i] = None
+        free_interval[0] += file_size
 
-            file_start = j
-            file_end = j
+        if free_interval[0] <= free_interval[1]:
+            new_free_size = free_interval[1] - free_interval[0] + 1
+
+            if new_free_size in free_intervals_size_map:
+
+                if len(free_intervals_size_map[new_free_size]) > 0:
+                    current_free_size_index = 0
+                    while True:
+                        if current_free_size_index >= len(free_intervals_size_map[new_free_size]):
+                            free_intervals_size_map[new_free_size].append(free_interval)
+                            break
+                        if free_intervals_size_map[new_free_size][current_free_size_index][0] > free_interval[0]:
+                            free_intervals_size_map[new_free_size].insert(current_free_size_index, free_interval)
+                            break
+                        current_free_size_index += 1
+                else:
+                    free_intervals_size_map[new_free_size].append(free_interval)
+            else:
+                free_intervals_size_map[new_free_size] = [free_interval]
 
     result = 0
     for i in range(len(file_layout)):
-        if file_layout[i] != None:
+        if file_layout[i] is not None:
             result += i*file_layout[i]
 
     print(result)
+
+if __name__ == "__main__":
+    test_time(main)
