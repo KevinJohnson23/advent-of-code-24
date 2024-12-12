d78f94d653d977bd4916ddd0b3e0e3748bf01832
Kevin Johnson <kevinJohnson190306@gmail.com>
2024-12-12 21:35:49 +0000

    Speed optimizations

--- Day12/Q1.py
+++ Day12/Q1.py
@@ -1,37 +1,31 @@
 from PerformanceTester.PerformanceTester import test_time
 
-def get_region(plots, x, y, plant, plot_positions):
-    if plots[y][x] != plant:
-        return
-
+def get_region(plots, x, y, plant, plot_positions, w, h):
     plot_positions.add((x, y))
 
-    if x > 0 and (x-1, y) not in plot_positions:
-        get_region(plots, x-1, y, plant, plot_positions)
-    if x < len(plots[0])-1 and (x+1, y) not in plot_positions:
-        get_region(plots, x+1, y, plant, plot_positions)
-    if y > 0 and (x, y-1) not in plot_positions:
-        get_region(plots, x, y-1, plant, plot_positions)
-    if y < len(plots)-1 and (x, y+1) not in plot_positions:
-        get_region(plots, x, y+1, plant, plot_positions)
+    xm, xp, ym, yp = x-1, x+1, y-1, y+1
 
-    return plot_positions
+    if x > 0 and (xm, y) not in plot_positions and plots[y][xm] == plant:
+        get_region(plots, xm, y, plant, plot_positions, w, h)
+    if x < w and (xp, y) not in plot_positions and plots[y][xp] == plant:
+        get_region(plots, x+1, y, plant, plot_positions, w, h)
+    if y > 0 and (x, ym) not in plot_positions and plots[ym][x] == plant:
+        get_region(plots, x, ym, plant, plot_positions, w, h)
+    if y < h and (x, yp) not in plot_positions and plots[yp][x] == plant:
+        get_region(plots, x, yp, plant, plot_positions, w, h)
 
 def get_adjacent(plot, x, y):
     adjacent = []
-    if (x-1, y) in plot:
-        adjacent.append((x-1, y))
-    if (x+1, y) in plot:
-        adjacent.append((x+1, y))
-    if (x, y-1) in plot:
-        adjacent.append((x, y-1))
-    if (x, y+1) in plot:
-        adjacent.append((x, y+1))
+    next_positions = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+    for next_x, next_y in next_positions:
+        if (next_x, next_y) in plot:
+            adjacent.append((next_x, next_y))
     return adjacent
 
 def main():
     with open("input.txt") as file:
-        plots = [list(line.strip()) for line in file]
+        plots = [line.strip() for line in file]
+    w, h = len(plots[0])-1, len(plots)-1
 
     plots_positions = []
     positions_added = dict()
@@ -39,7 +33,8 @@ def main():
         for x in range(len(plots[0])):
             if (x, y) in positions_added:
                 continue
-            plot_positions = get_region(plots, x, y, plots[y][x], set())
+            plot_positions = set()
+            get_region(plots, x, y, plots[y][x], plot_positions, w, h)
             plots_positions.append(plot_positions)
             for position in plot_positions:
                 positions_added[position] = True
@@ -48,10 +43,8 @@ def main():
     for plot in plots_positions:
         area = len(plot)
         perimeter = 0
-
         for x, y in plot:
             perimeter += 4 - len(get_adjacent(plot, x, y))
-
         price += area * perimeter
 
     print(price)
--- Day12/Q2.py
+++ Day12/Q2.py
@@ -1,36 +1,35 @@
 from PerformanceTester.PerformanceTester import test_time
 
-def get_region(plots, x, y, plant, plot_positions):
-    if plots[y][x] != plant:
-        return
+def get_region(plots, x, y, plant, plot_positions, w, h):
+    plot_positions.add((x, y))
 
-    plot_positions.append((x, y))
+    xm, xp, ym, yp = x-1, x+1, y-1, y+1
 
-    if x > 0 and (x-1, y) not in plot_positions:
-        get_region(plots, x-1, y, plant, plot_positions)
-    if x < len(plots[0])-1 and (x+1, y) not in plot_positions:
-        get_region(plots, x+1, y, plant, plot_positions)
-    if y > 0 and (x, y-1) not in plot_positions:
-        get_region(plots, x, y-1, plant, plot_positions)
-    if y < len(plots)-1 and (x, y+1) not in plot_positions:
-        get_region(plots, x, y+1, plant, plot_positions)
-
-    return plot_positions
+    if x > 0 and (xm, y) not in plot_positions and plots[y][xm] == plant:
+        get_region(plots, xm, y, plant, plot_positions, w, h)
+    if x < w and (xp, y) not in plot_positions and plots[y][xp] == plant:
+        get_region(plots, x+1, y, plant, plot_positions, w, h)
+    if y > 0 and (x, ym) not in plot_positions and plots[ym][x] == plant:
+        get_region(plots, x, ym, plant, plot_positions, w, h)
+    if y < h and (x, yp) not in plot_positions and plots[yp][x] == plant:
+        get_region(plots, x, yp, plant, plot_positions, w, h)
 
 def main():
     with open("input.txt") as file:
-        plots = [list(line.strip()) for line in file]
+        plots = [line.strip() for line in file]
+    w, h = len(plots[0]) - 1, len(plots) - 1
 
     plots_positions = []
-    positions_added = set()
+    positions_added = dict()
     for y in range(len(plots)):
         for x in range(len(plots[0])):
             if (x, y) in positions_added:
                 continue
-            plot_positions = get_region(plots, x, y, plots[y][x], [])
+            plot_positions = set()
+            get_region(plots, x, y, plots[y][x], plot_positions, w, h)
             plots_positions.append(plot_positions)
             for position in plot_positions:
-                positions_added.add(position)
+                positions_added[position] = True
 
     price = 0
     for plot in plots_positions:
@@ -39,25 +38,27 @@ def main():
         corners = []
 
         for x, y in plot:
+            xm, xp, ym, yp = x-1, x+1, y-1, y+1
+
             #Convex corner
-            if (x-1, y) not in plot and (x, y-1) not in plot:
+            if (xm, y) not in plot and (x, ym) not in plot:
                 corners.append((x, y))
-            if (x+1, y) not in plot and (x, y+1) not in plot:
-                corners.append((x+1, y+1))
-            if (x, y-1) not in plot and (x+1, y) not in plot:
-                corners.append((x+1, y))
-            if (x-1, y) not in plot and (x, y+1) not in plot:
-                corners.append((x, y+1))
+            if (xp, y) not in plot and (x, yp) not in plot:
+                corners.append((xp, yp))
+            if (x, ym) not in plot and (xp, y) not in plot:
+                corners.append((xp, y))
+            if (xm, y) not in plot and (x, yp) not in plot:
+                corners.append((x, yp))
 
             #Concave corner
-            if (x-1, y) in plot and (x, y-1) in plot and (x-1, y-1) not in plot:
+            if (xm, y) in plot and (x, ym) in plot and (xm, ym) not in plot:
                 corners.append((x, y))
-            if (x+1, y) in plot and (x, y-1) in plot and (x+1, y-1) not in plot:
-                corners.append((x+1, y))
-            if (x+1, y) in plot and (x, y+1) in plot and (x+1, y+1) not in plot:
-                corners.append((x+1, y+1))
-            if (x-1, y) in plot and (x, y+1) in plot and (x-1, y+1) not in plot:
-                corners.append((x, y+1))
+            if (xp, y) in plot and (x, ym) in plot and (xp, ym) not in plot:
+                corners.append((xp, y))
+            if (xp, y) in plot and (x, yp) in plot and (xp, yp) not in plot:
+                corners.append((xp, yp))
+            if (xm, y) in plot and (x, yp) in plot and (xm, yp) not in plot:
+                corners.append((x, yp))
 
         sides = len(corners)
         price += area * sides
