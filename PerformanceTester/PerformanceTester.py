46104ca0d685afcb50f9f94612b8484232bc172e
Kevin Johnson <kevinJohnson190306@gmail.com>
2024-12-12 21:55:41 +0000

    Increased iterations of execution for performance testing

--- PerformanceTester/PerformanceTester.py
+++ PerformanceTester/PerformanceTester.py
@@ -1,5 +1,6 @@
 from timeit import timeit
 
-def test_time(func):
-    execution_time = timeit(func, number=1)
-    print(f"Execution time: {execution_time*1000:.3f} ms")
+def test_time(func, n=1000):
+    execution_time = timeit(func, number=n)
+    average_time = execution_time / n
+    print(f"Average execution time: {average_time*1000:.3f} ms")
