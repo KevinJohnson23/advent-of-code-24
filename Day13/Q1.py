508dfa55814ce302856463ee5ed14832a972be71
Kevin Johnson <kevinJohnson190306@gmail.com>
2024-12-13 16:48:36 +0000

    Day 13 solution

---
+++ Day13/Q1.py
@@ -0,0 +1,34 @@
+from PerformanceTester.PerformanceTester import test_time
+
+def main():
+    with open("input.txt") as file:
+        file_content = file.read()
+    file_content = file_content.replace("Button A: ", "")
+    file_content = file_content.replace("Button B: ", "")
+    file_content = file_content.replace("Prize: ", "")
+    file_content = file_content.replace("X", "")
+    file_content = file_content.replace("Y", "")
+    file_content = file_content.replace("+", "")
+    file_content = file_content.replace("=", "")
+    file_content = file_content.replace(" ", "")
+
+    tokens = 0
+
+    for game in file_content.split("\n\n"):
+        a, b, prize = game.split("\n")
+        xa, ya = [int(c) for c in a.split(",")]
+        xb, yb = [int(c) for c in b.split(",")]
+        xp, yp = [int(c) for c in prize.split(",")]
+
+        u = (xp * yb - yp * xb) / (xa * yb - ya * xb)
+        if int(u) != u:
+            continue
+        u = int(u)
+        v = (xp - u * xa) // xb
+
+        tokens += u * 3 + v
+
+    print(tokens)
+
+if __name__ == "__main__":
+    test_time(main)
